447bdac07929a432dcf802986bb122a906216363
Alex Schenk <134595144+alx-sch@users.noreply.github.com>
2025-12-23 09:16:54 +0000

    [fix] all suggestions (hoefully) and added linting to CI

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,4 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap().catch((err) => console.error(err));
+bootstrap().catch((err) => {
+  console.error('Fatal Error during startup:', err);
+  process.exit(1);
+});
