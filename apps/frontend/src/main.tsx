14fb6c17445a643885b904c50ad39f704f3f3294
Dovy Mus <dovy.mus@proton.me>
2026-03-18 14:52:19 +0100

    feat(fonts): add fonts locally

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/space-grotesk/500.css';
+import '@fontsource/space-grotesk/600.css';
+import '@fontsource/space-grotesk/700.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { ThemeProvider } from '@/providers/theme-provider';
